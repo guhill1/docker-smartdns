8ee3d24ca79cf1295489ce933f5638d3ca7f0a01
guhill1 <124128440+guhill1@users.noreply.github.com>
2023-09-09 13:59:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ ! -f /etc/smartdns/smartdns.conf ]; then
-	mkdir -p /etc/smartdns
-	cp -u /smartdns.conf /etc/smartdns/smartdns.conf
-fi
+#if [ ! -f /etc/smartdns/smartdns.conf ]; then
+#	mkdir -p /etc/smartdns
+#	cp -u /smartdns.conf /etc/smartdns/smartdns.conf
+#fi
 /usr/sbin/smartdns -f -x -c /etc/smartdns/smartdns.conf
